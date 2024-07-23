11d2b0b61497e566e3f38085c90da1ff183fe24a
Ampelio Plascencia <ampeliodev@hotmail.com>
2024-07-23 08:18:39 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'noticias-reto',
+  webDir: 'www'
+};
+
+export default config;
